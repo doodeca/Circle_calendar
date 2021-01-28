aff8ba9dd600d12d1a0946d679dd25ce482b6e09
doodeca <76733840+doodeca@users.noreply.github.com>
2021-01-28 10:45:54 +0100

    Fixed hall sensor problems

--- Code/Kalender.ino
+++ Code/Kalender.ino
@@ -2,20 +2,20 @@
 #include <Stepper.h>
 #include <RTClib.h>
 
-RTC_DS3231 rtc;        // RTC module uses DS3231
+RTC_DS3231 rtc;        // de RTC module maakt gebruik van een DS3231 IC
 
-#define OMWENTELING 2038  // number of steps needed to make one revolution
+#define OMWENTELING 2038  // aantal stappen dat de uitgaande as eenmaal rond laat draaien
 
-#define HALL_dagNummer 3  // schematic: HALL_0, on PCB: U2, RV2
-#define HALL_maandNaam 4  // schematic: HALL_1, on PCB: U1, RV1
+#define HALL_dagNummer 4  // op print: HALL_0, U2, RV2
+#define HALL_maandNaam 3  // op print: HALL_1, U1, RV1
 
 const int COUNTERCLOCKWISE = -1;
 
-// stepper sequence = orange, purple, yellow, blue
+// stepper sequence = oranje, paars, geel, blauw
 Stepper dagNummer = Stepper(OMWENTELING, 12, 10, 11, 9);  // STEPPER 0
-Stepper maandNaam = Stepper(OMWENTELING, 5, 7, 6, 8);     // STEPPER_1
+Stepper maandNaam = Stepper(OMWENTELING, 5, 7, 6, 8);   // STEPPER_1
 
-// struct for keeping this and next day
+// struct voor het bijhouden van huidige en komende dag
 typedef struct {
   uint8_t Nummer;
   uint8_t Maand;
@@ -25,13 +25,27 @@ typedef struct {
 
   Datum datum;
 
+void schuifDag(){
+  while(digitalRead(HALL_dagNummer)){
+    dagNummer.step(COUNTERCLOCKWISE);}
+  while(!digitalRead(HALL_dagNummer)){
+    dagNummer.step(COUNTERCLOCKWISE);}
+}
+
+void schuifMaand(){
+  while(digitalRead(HALL_maandNaam)){
+    maandNaam.step(COUNTERCLOCKWISE);}
+  while(!digitalRead(HALL_maandNaam)){
+    maandNaam.step(COUNTERCLOCKWISE);}
+}
+
 void setup() {
   rtc.begin();
   
-// Uncomment to set RTC date/time
+// Uncomment om tijd in RTC in te stellen
   // rtc.adjust(DateTime(F(__DATE__), F(__TIME__)));
   
-// get current time from RTC
+// huidige tijd ophalen uit RTC
   DateTime now  = rtc.now();
   datum.Nummer  = now.day();
   datum.Maand   = now.month();
@@ -39,58 +53,46 @@ void setup() {
   pinMode(HALL_dagNummer, INPUT_PULLUP);
   pinMode(HALL_maandNaam, INPUT_PULLUP);
   
-// set stepper speed
+// snelheid stappenmotoren
   dagNummer.setSpeed(15);
   maandNaam.setSpeed(15);
+
+  while(digitalRead(HALL_maandNaam)){
+    maandNaam.step(COUNTERCLOCKWISE);
+  }
+  while(digitalRead(HALL_dagNummer)){
+    dagNummer.step(COUNTERCLOCKWISE);
+  }
+
 }
 
 void loop(){
-  // get current day
+  
+  while(1){
+  // huidige dag ophalen
   DateTime now = rtc.now();
   datum.volgendNummer = now.day();
   
   if(datum.volgendNummer != datum.Nummer){
-    // if the date has changed
+    // als datum veranderd is
     datum.volgendMaand = now.month();
     
-    if(datum.volgendNummer == 1 && datum.Nummer >= 28){    // if the new month started
-      int aantal_plekken = 31 - datum.Nummer;    // number of places that should be skipped
-      
+    // als een nieuwe maand begint, nummer ring verder doorschuiven
+    if(datum.volgendNummer == 1 && datum.Nummer >= 28){
+      int aantal_plekken = 31 - datum.Nummer; // aantal nummers dat doorgeschoven moeten worden
       while(aantal_plekken != 0){
-        int bewegen = 1;
-        
-        while(bewegen){
-          maandNaam.step(COUNTERCLOCKWISE);
-          
-          if(digitalRead(HALL_maandNaam) == LOW){
-            while(digitalRead(HALL_maandNaam) == LOW){
-              maandNaam.step(COUNTERCLOCKWISE);
-            }
-            aantal_plekken--;
-            bewegen = 0; 
+          schuifDag();           
+          aantal_plekken--;
           }
-        }
-      }
-    }
-    
-    if(datum.volgendMaand != datum.Maand){
-      int bewegen = 1;
-      
-      while(bewegen){
-        maandNaam.step(COUNTERCLOCKWISE);
-        
-        if(digitalRead(HALL_maandNaam) == LOW){
-          while(digitalRead(HALL_maandNaam) == LOW){
-            maandNaam.step(COUNTERCLOCKWISE);
-          }
-          bewegen = 0; 
-        }
-      }
-    }
+       } 
+   if(datum.volgendMaand != datum.Maand){
+     schuifMaand();
+   }
   }
-  // reset variables
+    
+  // variabelen resetten
   datum.Nummer = datum.volgendNummer;
   datum.Maand  = datum.volgendMaand;
   
-  LowPower.powerDown(SLEEP_8S, ADC_OFF, BOD_OFF);   // sleep for 8 seconds
-}
+  LowPower.powerDown(SLEEP_8S, ADC_OFF, BOD_OFF);   // LPM
+}}
